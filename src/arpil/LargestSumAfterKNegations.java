b8062dd8d25e6b9c72e8eff66d2c205c17f7fc9c
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-04-29 16:55:38 +0500

    add LargestSumAfterKNegations

---
+++ src/arpil/LargestSumAfterKNegations.java
@@ -0,0 +1,25 @@
+package arpil;
+
+public class LargestSumAfterKNegations {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int j = index(nums);
+            nums[j] *= -1;
+        }
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public int index(int[] nums){
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                i = j;
+            }
+        }
+        return i;
+    }
+}
