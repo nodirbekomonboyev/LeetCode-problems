80db34efd09c8b81ec864dc71688a2acf4d302ba
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-05-02 13:47:50 +0500

    add FindMaxK

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import arpil.*;
+import may.FindMaxK;
 import may.ReversePrefix;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ReversePrefix reversePrefix = new ReversePrefix();
-        System.out.println(reversePrefix.reversePrefix("asdgfhjafsgadfsgh", 'd'));
+        FindMaxK findMaxK = new FindMaxK();
+        System.out.println(findMaxK.findMaxK(new int[]{-1, 0, 4, 3, 7, 5, -2,}));
 
     }
 }
