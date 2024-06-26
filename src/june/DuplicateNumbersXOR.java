ed43357f0f3b624ffc05784089f6c3d8b765591b
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-06-26 23:12:48 +0500

    add DuplicateNumbersXOR

---
+++ src/june/DuplicateNumbersXOR.java
@@ -0,0 +1,15 @@
+package june;
+
+public class DuplicateNumbersXOR {
+    public int duplicateNumbersXOR(int[] nums) {
+        int[] occrCount = new int[50];
+        int xorResult = 0;
+        for(int num : nums){
+            occrCount[num-1]++;
+            if(occrCount[num-1] % 2 == 0){
+                xorResult ^= num;
+            }
+        }
+        return xorResult;
+    }
+}
