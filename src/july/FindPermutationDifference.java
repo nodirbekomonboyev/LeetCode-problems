2b2c627c8fef5dacb2fb7d0112972de73850236e
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-07-16 16:00:44 +0500

    add FindPermutationDifference

---
+++ src/july/FindPermutationDifference.java
@@ -0,0 +1,11 @@
+package july;
+
+public class FindPermutationDifference {
+    public int findPermutationDifference(String s, String t) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            ans += Math.abs(i - t.indexOf(s.charAt(i)));
+        }
+        return ans;
+    }
+}
