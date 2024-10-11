4c37751dc91a39e3fd665ca9ba991a82422a7157
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-10-11 21:33:11 +0500

    add ArrayPairSum

---
+++ src/october/ArrayPairSum.java
@@ -0,0 +1,15 @@
+package october;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ArrayPairSum {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
