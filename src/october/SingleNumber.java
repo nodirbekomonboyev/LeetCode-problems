acab6fe6e0b477e3ba304e492f455a8d6dfcfd96
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-11-04 20:56:34 +0500

    add Compress and SingleNumber, FindDifference

---
+++ src/october/SingleNumber.java
@@ -0,0 +1,11 @@
+package october;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i : nums){
+            ans ^= i;
+        }
+        return ans;
+    }
+}
