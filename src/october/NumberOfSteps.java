a9440b76f1ecd3ccf5fe5cd3f8a1dd095d3d5c69
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-11-05 14:06:14 +0500

    add MinOperations, NumberOfSteps and PivotIndex

---
+++ src/october/NumberOfSteps.java
@@ -0,0 +1,17 @@
+package october;
+
+public class NumberOfSteps {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while(num > 0){
+            if(num % 2 == 0){
+                num /= 2;
+            }else {
+                num -= 1;
+
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
