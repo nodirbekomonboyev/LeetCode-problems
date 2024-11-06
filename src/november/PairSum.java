76d06533cf4a3dfc709b217e185766e637a7faf3
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-11-07 01:05:31 +0500

    add PairSum

---
+++ src/november/PairSum.java
@@ -0,0 +1,20 @@
+package november;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairSum {
+    public int pairSum(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+        int max = 0;
+        int n = list.size();
+        for (int i = 0; i < n / 2 ; i++) {
+            max = Math.max(max, list.get(i) + list.get(n - i - 1));
+        }
+        return max;
+    }
+}
