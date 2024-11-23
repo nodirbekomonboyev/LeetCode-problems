ccc0e8fa7e8dd0e4c4a80e10c65ec4d9ed0a860f
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-11-24 02:01:16 +0500

    add OrangeRotting and PredictPartyVictory

---
+++ src/november/OrangeRotting.java
@@ -0,0 +1,41 @@
+package november;
+
+public class OrangeRotting {
+    public int orangesRotting(int[][] grid) {
+        int point = 0;
+        for (int[] ints : grid) {
+            for (int i : ints) {
+                if(i  == 1){
+                    point++;
+                }
+            }
+        }
+        if (point == 0){
+            return 0;
+        }
+        int minute = 0;
+        int progress = 1;
+        while(progress == 0) {
+            progress = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[i].length; j++) {
+                    if(grid[i][j] == 2){
+                        int n = i;
+                        int m = j;
+                        if(i == 0 && j == 0){
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    /*
+
+      [[0,2,2]]
+      [[2,2,2,1,1]]
+      [[2,2],[1,1],[0,0],[2,0]]
+
+      */
+}
---
+++ src/november/PredictPartyVictory.java
@@ -0,0 +1,59 @@
+package november;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class PredictPartyVictory {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> radiant = new ArrayDeque<>();
+        Queue<Integer> dire = new ArrayDeque<>();
+        char[] chars = senate.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i] == 'R'){
+                radiant.add(i);
+            } else {
+                dire.add(i);
+            }
+        }
+        int i = senate.length();
+        while (!radiant.isEmpty() && !dire.isEmpty()){
+            if(radiant.peek() < dire.peek()){
+                radiant.add(i++);
+            } else {
+                dire.add(i++);
+            }
+            radiant.poll();
+            dire.poll();
+        }
+        return (radiant.isEmpty()) ? "Dire" : "Radiant";
+    }
+}
+
+/*
+* DRRDRDRDRDDRDRDR
+* RRDRDRDRDDRDRDd
+* RDRDRDRDDRDRDr
+* DRDRDRDDRDRrr
+* RDRDRDDRDRrd
+* DRDRDDRDRrr
+* RDRDDRDRrd
+* DRDDRDRrr
+* RDDRDRrd
+* DDRDRrr
+* DRDRrd
+* RDRdd
+* DRdr
+* Rdd
+* dr
+* d
+* */
+
+/*
+* DRRDRDRDRDDRDRDR
+* DxRxRxRxRxDxDxDx
+* DxxxRxRxRxxxDxDx
+* DxxxxxRxRxxxxxDx
+* DxxxxxxxRxxxxxxx
+* xxxxxxxxRxxxxxxx
+*
+* */
