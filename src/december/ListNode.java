e49cc9de384e453efe3b5fe69819ab7fa3dacb5a
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-12-05 00:45:34 +0500

    add ListNode

---
+++ src/december/ListNode.java
@@ -0,0 +1,15 @@
+package december;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    public ListNode(int val) {
+        this.val = val;
+    }
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
