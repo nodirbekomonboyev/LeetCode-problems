4576dadaa3e7431762d4bf09a61e02d471e20d13
nodirbekomonboyev <omonboevnodir77@gmail.com>
2025-04-24 02:23:20 +0500

    Problem1636

---
+++ src/year2025/Prbolem1636.java
@@ -0,0 +1,37 @@
+package year2025;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Prbolem1636 {
+
+    // https://leetcode.com/problems/sort-array-by-increasing-frequency/
+
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        Integer[] numsObj = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numsObj[i] = nums[i];
+        }
+
+        Arrays.sort(numsObj, (a, b) -> {
+            if (freq.get(a).equals(freq.get(b))) {
+                return Integer.compare(b, a);
+            }
+            return Integer.compare(freq.get(a), freq.get(b));
+        });
+
+        // Convert numsObj back to a primitive array to match
+        // return type.
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = numsObj[i];
+        }
+
+        return nums;
+    }
+}
